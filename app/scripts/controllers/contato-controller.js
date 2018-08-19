9498b0ead390022af58ca102894510472426e610
João Tavares Filho Segundo <tauvares@gmail.com>
2018-08-19 02:17:00 -0300

    Módulo de contato com e-mail funcionando

--- app/scripts/controllers/contato-controller.js
+++ app/scripts/controllers/contato-controller.js
@@ -1,18 +1,28 @@
 'use strict';
 angular.module('grande-colorado-pub')
-  .controller('ContatoController', ['$scope', 'Categoria',
-    function($scope, Categoria) {      
-      $scope.enviarEmail = function() {
-        //$scope.contato = contato;
+  .controller('ContatoController', ['$scope', 'Categoria', 'ngDialog',
+    function ($scope, Categoria, ngDialog) {
+      $scope.enviarEmail = function () {
         var mensagem = {
-          name : $scope.pessoa,
-          address : $scope.emailaddress,
-          phone : $scope.telephone,
-          subject : $scope.assunto,
-          msg : $scope.mensagem
+          name: $scope.pessoa,
+          address: $scope.emailaddress,
+          phone: $scope.telephone,
+          subject: $scope.assunto,
+          msg: $scope.mensagem
         };
         Categoria.enviaEmail(mensagem);
+        var textoFormatado = '\
+              <div class="ngdialog-message">\
+                <div><h3>E-mail enviado com sucesso</h3></div>' +
+              '<div class="ngdialog-buttons">\
+                    <button type="button" class="ngdialog-button" ng-click=confirm("OK")>OK</button>\
+                </div>';
+        ngDialog.openConfirm({
+          template: textoFormatado,
+          plain: 'true'
+        });
+
       };
     }
   ])
-;
+  ;
